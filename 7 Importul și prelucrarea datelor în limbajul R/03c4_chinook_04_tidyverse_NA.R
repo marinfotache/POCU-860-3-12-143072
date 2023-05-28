f7288927c3bc2fedb03ffe74aecd23039c59ef7e
MarinFotache <marinfotache@gmail.com>
2023-05-28 13:57:27 +0300

    Add files via upload

---
+++ 7 Importul și prelucrarea datelor în limbajul R/03c4_chinook_04_tidyverse_NA.R
@@ -0,0 +1,162 @@
+############################################################################
+###      Titlul proiectului: Educație și dezvoltare în era digitală      ###
+###                   Cod proiect: POCU/860/3/12/143072                  ###
+###         Curs: Introducere în analiza datelor de mari dimensiuni      ###
+###                       Perioada: mai-iunie 2023                       ###
+############################################################################
+
+##############################################################################
+##        Studiu de caz: Interogări tidyverse pentru baza de date `chinook`
+##############################################################################
+## 			tidyverse04: Tratamentul valorilor lipsă
+##############################################################################
+## ultima actualizare: 2023-05-28
+
+library(tidyverse)
+library(lubridate)
+
+setwd('/Users/marinfotache/OneDrive/POCU-860-3-12-143072/7 Importul și prelucrarea datelor în limbajul R')
+load("chinook.RData")
+
+
+##############################################################################
+###                                    is.na()
+##############################################################################
+
+##############################################################################
+##            Care sunt clienții individuali (non-companii)
+##############################################################################
+
+temp <- customer %>%
+        filter (is.na(company))
+
+
+##############################################################################
+##               Care sunt clienții care reprezintă companii
+##############################################################################
+
+temp <- customer %>%
+        filter (!is.na(company))
+
+
+
+##############################################################################
+##      Care sunt piesele de pe albumele formației `Black Sabbath`
+##                  cărora nu li se cunoaște compozitorul
+##############################################################################
+
+# solutia bazata pe functia `is.na`
+temp <- artist %>%
+     filter (name == 'Black Sabbath') %>%
+     select (-name) %>%
+     inner_join(album) %>%
+     inner_join(track) %>%
+     select (name, composer) %>%
+     filter (is.na(composer))
+
+
+# a doua solutia bazata pe functia `is.na`
+temp <- artist %>%
+     filter (name == 'Black Sabbath') %>%
+     inner_join(album) %>%
+     inner_join(track, by = c('albumid' = 'albumid')) %>%
+     transmute (track_name = name.y, composer) %>%
+     filter (is.na(composer))
+
+
+##############################################################################
+##   Să se afișeze, sub formă de șir de caractere, orașele din care provin
+## clienții (pentru a elimina confuziile, numele orașului trebuie concatenat
+## cu statul și tara din care face parte orașul respectiv)
+##############################################################################
+
+
+
+#############################################################################
+##  Afisati clientii in ordinea tarilor; pentru cei din tari non-federative,
+##  la atributul `state`, in locul valorii NULL, afisati `-`
+#############################################################################
+
+# solutie cu `if_else`
+temp <- customer %>%
+    select (customerid:lastname, state) %>%
+    mutate(state2 = if_else(is.na(state), '-', state))
+
+
+# solutie cu `case_when`
+temp <- customer %>%
+    select (customerid:lastname, state) %>%
+    mutate(state2 = case_when(
+            is.na(state) ~ '-',
+            TRUE ~ state))
+
+
+# solutie cu `coalesce` - vezi mai jos
+#
+#
+
+
+##############################################################################
+###                               COALESCE
+##############################################################################
+
+##############################################################################
+## Afișați clienții în ordinea țărilor; pentru cei din țări non-federative,
+##   la atributul `state`, în locul valorii NULL, afișati `-`
+##############################################################################
+
+temp <- customer %>%
+    select (customerid:lastname, state) %>%
+    mutate(state2 = coalesce(state, '-'))
+
+
+##############################################################################
+## Să se afișeze, în ordine alfabetică, toate titlurile pieselor de pe
+##  albumele formației `Black Sabbath`, împreuna cu autorii (compozitorii) lor;
+##  acolo unde compozitorul nu este specificat (NULL), să se afișeze
+##  `COMPOZITOR NECUNOSCUT`
+##############################################################################
+
+# solutie bazata pe functia `coalesce`
+temp <- artist %>%
+     filter (name == 'Black Sabbath') %>%
+     select (-name) %>%
+     inner_join(album) %>%
+     inner_join(track) %>%
+     transmute (name, composer = coalesce(composer, 'COMPOZITOR NECUNOSCUT')) %>%
+     arrange(name)
+
+
+
+
+
+##############################################################################
+##               Probleme de rezolvat la curs/laborator/acasa
+##############################################################################
+
+
+# ############################################################################
+# Sa se afiseze, sub forma de sir de caractere, orasele din care provin
+# clientii (pentru a elimina confuziile, numele orasului trebuie concatenat
+# cu statul si tara din care face parte orasul respectiv)
+
+temp <- customer %>%
+    select (city, state, country) %>%
+    mutate (city_string1 = paste(city, coalesce(state, '-'), country)) %>%
+    distinct(.)
+
+
+
+
+
+############################################################################
+# ##Afisati toate facturile (tabela `invoice), completand eventualele valori NULL
+# ##  ale atributului `billingstate` cu valoarea tributului `billing city` de pe
+# ##  aceasi linie
+#
+
+
+
+##############################################################################
+##             La ce întrebări răspund următoarele interogări ?
+##############################################################################
---
+++ 7 Importul și prelucrarea datelor în limbajul R/03c5_chinook_05_tidyverse_functii_agregat_fara_grupare.R
@@ -0,0 +1,285 @@
+############################################################################
+###      Titlul proiectului: Educație și dezvoltare în era digitală      ###
+###                   Cod proiect: POCU/860/3/12/143072                  ###
+###         Curs: Introducere în analiza datelor de mari dimensiuni      ###
+###                       Perioada: mai-iunie 2023                       ###
+############################################################################
+
+##############################################################################
+##        Studiu de caz: Interogări tidyverse pentru baza de date `chinook`
+##        Case study: tidyverse queries for `chinook` database
+##############################################################################
+## 	  tidyverse05: Funcții agregat (count, count distinct, ...) fără grupare
+## 		tidyverse05: Aggregate functions without gruping
+##############################################################################
+## ultima actualizare: 2023-05-28
+
+library(tidyverse)
+library(lubridate)
+
+setwd('/Users/marinfotache/OneDrive/POCU-860-3-12-143072/7 Importul și prelucrarea datelor în limbajul R')
+load("chinook.RData")
+
+
+##############################################################################
+##                   Câți artiști sunt în baza de date?
+##############################################################################
+
+# Solutie eronata!!! (aceasta solutie numara de cate ori apare fiecare valoare
+# a `artistid` in data frame-ul `artist`)
+temp <- artist %>%
+     count(artistid)
+
+
+# Solutie 1 - `count()`
+temp <- artist %>%
+     count()
+
+
+# Solutie 2 - `summarise`, `n()`
+temp <- artist %>%
+     summarise(n_of_artists = n())
+
+
+# Solutie 3 - `tally()`
+temp <- artist %>%
+     tally()
+
+
+
+##############################################################################
+##				                     Câți clienți au fax?
+##############################################################################
+
+# sol 1
+temp <- customer %>%
+        filter(!is.na(fax)) %>%
+        tally()
+
+# sol 2
+temp <- customer %>%
+        filter(!is.na(fax)) %>%
+        summarise(n_of_customers_with_faxes = n())
+
+
+
+
+##############################################################################
+##				Pentru câți artiști există măcar un album în baza de date?
+##############################################################################
+
+
+# solutie bazata pe `n_distinct`
+temp <- album %>%
+     summarise (n = n_distinct(artistid))
+
+
+# solutie bazata pe `semi-join` si `count`
+temp <- artist %>%
+     semi_join(album) %>%
+     count()
+
+
+# solutie bazata pe `semi-join` si `summarise`+`n()`
+temp <- artist %>%
+     semi_join(album) %>%
+     summarise (n = n())
+
+
+
+##############################################################################
+##               Din câte țări sunt clienții companiei?
+##############################################################################
+
+# solutie bazata pe `n_distinct`
+temp <- customer %>%
+     summarise (n = n_distinct(country))
+
+
+
+
+############################################################################
+##               Din cate orașe sunt clienții companiei?
+############################################################################
+
+# solutie bazata pe `n_distinct`
+temp <- customer %>%
+     summarise (n = n_distinct(paste(city, state,
+                                     country, sep = ' - ')))
+
+
+
+##############################################################################
+##       Câte secunde are albumul `Achtung Baby` al formației `U2`?
+##############################################################################
+
+# solutie bazata pe `summarise` si `sum`
+temp <- album %>%
+     filter (title == 'Achtung Baby') %>%
+     semi_join(artist %>%
+                    filter (name == 'U2')) %>%
+     inner_join(track) %>%
+     summarise(durata = sum(milliseconds / 1000))
+
+
+
+##############################################################################
+##              Care este durata medie (în secunde) a pieselor
+##              de pe albumul `Achtung Baby` al formației `U2`
+##############################################################################
+
+# solutie bazata pe `summarise` si `mean`
+temp <- album %>%
+     filter (title == 'Achtung Baby') %>%
+     semi_join(artist %>%
+                    filter (name == 'U2')) %>%
+     inner_join(track) %>%
+     summarise(durata_medie = mean(milliseconds / 1000))
+
+
+
+##############################################################################
+##          Care este durata medie a pieselor formației `U2`
+##############################################################################
+
+# solutie bazata pe `summarise` si `mean`
+temp <- album %>%
+     semi_join(artist %>%
+                    filter (name == 'U2')) %>%
+     inner_join(track) %>%
+     summarise(durata_medie = mean(milliseconds / 1000))
+
+
+
+##############################################################################
+##			Care este durata medie a pieselor formației `Pink Floyd`,
+##                    exprimată în minute și secunde
+##############################################################################
+
+# solutie bazata pe `summarise`, `mean` si `lubridate::seconds_to_period`
+temp <- album %>%
+     semi_join(artist %>%
+                    filter (name == 'Pink Floyd')) %>%
+     inner_join(track) %>%
+     summarise(durata_medie =
+          trunc(lubridate::seconds_to_period(mean(milliseconds / 1000))))
+
+
+
+##############################################################################
+##                     În ce zi a fost prima vânzare?
+##############################################################################
+
+# solutie cu functia `min`
+temp <- invoice %>%
+    summarise(first_day = min(invoicedate))
+
+
+# solutie cu optiunea `head`
+temp <- invoice %>%
+    arrange (invoicedate) %>%
+    head(1)  %>%
+    transmute (first_day = invoicedate)
+
+# solutie cu optiunea `tail`
+temp <- invoice %>%
+    arrange (desc(invoicedate)) %>%
+    tail(1)  %>%
+    transmute (first_day = invoicedate)
+
+
+# solutie cu optiunea `top` (atentie la `-1`!!!)
+temp <- invoice %>%
+    distinct(invoicedate) %>%
+    top_n (-1, invoicedate) %>%
+    transmute (first_day = invoicedate)
+
+
+# solutie cu `slice`
+temp <- invoice %>%
+    distinct(invoicedate) %>%
+    arrange(invoicedate)  %>%
+    slice(1)
+
+
+# alta solutie cu `slice`
+temp <- invoice %>%
+    distinct(invoicedate) %>%
+    arrange(desc(invoicedate))  %>%
+     slice(nrow(.))
+
+
+# solutie cu filtrare ce foloseste `rownum()`
+temp <- invoice %>%
+    distinct(invoicedate) %>%
+    arrange(invoicedate)  %>%
+    filter (row_number() <= 1)
+
+
+
+##############################################################################
+##                      În ce dată a fost ultima vanzare?
+##############################################################################
+
+temp <- invoice %>%
+     summarise(last_day = max(invoicedate))
+
+
+# solutie cu optiunea `head`
+temp <- invoice %>%
+    arrange (desc(invoicedate)) %>%
+    head(1)  %>%
+    transmute (last_day = invoicedate)
+
+
+# solutie cu optiunea `tail`
+temp <- invoice %>%
+    arrange (invoicedate) %>%
+    tail(1)  %>%
+    transmute (last_day = invoicedate)
+
+
+# solutie cu optiunea `top_n`
+temp <- invoice %>%
+    distinct(invoicedate) %>%
+    top_n (1, invoicedate) %>%
+    transmute (first_day = invoicedate)
+
+
+
+
+##############################################################################
+##               Probleme de rezolvat la curs/laborator/acasa
+##############################################################################
+
+#
+##############################################################################
+##                Cate piese sunt stocate în baza de date?
+##############################################################################
+
+# ##Care este data primei angajari in companie
+#
+# ##Cate piese sunt pe playlistul `Grunge`?
+#
+# ##Cati subordonati are, in total (pe toate nivelurile) angajatul xxxxxx?
+#
+
+
+
+##############################################################################
+##             La ce întrebări răspund următoarele interogări ?
+##############################################################################
+
+
+##
+invoice %>%
+        summarise(first_day = min(invoicedate), last_day = max(invoicedate)) %>%
+        mutate (range = last_day - first_day) %>%
+        transmute(range)
+
+##
+temp <- track %>%
+        filter (milliseconds / 1000 > mean(milliseconds/1000))
+
+
+mean(track$milliseconds/1000)
