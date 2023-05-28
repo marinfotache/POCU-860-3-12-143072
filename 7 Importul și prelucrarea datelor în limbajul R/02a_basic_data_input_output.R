5a9dd1b671a040c221908b8f9d61e7a8a1e8efec
MarinFotache <marinfotache@gmail.com>
2023-05-28 13:43:48 +0300

    Add files via upload

--- 7 Importul și prelucrarea datelor în limbajul R/02a_basic_data_input_output.R
+++ 7 Importul și prelucrarea datelor în limbajul R/02a_basic_data_input_output.R
@@ -11,8 +11,6 @@
 ###
 ############################################################################
 ###                    2a. Data Input from Most Common Sources           ###
-### See also the presentation:
-### https://github.com/marinfotache/Data-Processing-Analysis-Science-with-R/blob/master/02%20Basic%20Data%20Import%20and%20Export/02a_basic_data_input_output.pptx
 ############################################################################
 ## last update: 2023-05-28
 
