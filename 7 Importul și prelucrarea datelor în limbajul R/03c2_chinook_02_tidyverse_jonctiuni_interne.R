b83ec80873d312cd9cc5ee12e62d282f32365edd
MarinFotache <marinfotache@gmail.com>
2023-05-28 13:52:13 +0300

    Add files via upload

---
+++ 7 Importul și prelucrarea datelor în limbajul R/03c2_chinook_02_tidyverse_jonctiuni_interne.R
@@ -0,0 +1,282 @@
+############################################################################
+###      Titlul proiectului: Educație și dezvoltare în era digitală      ###
+###                   Cod proiect: POCU/860/3/12/143072                  ###
+###         Curs: Introducere în analiza datelor de mari dimensiuni      ###
+###                       Perioada: mai-iunie 2023                       ###
+############################################################################
+
+##############################################################################
+##        Studiu de caz: Interogări tidyverse pentru baza de date `chinook`
+##############################################################################
+## 			tidyverse02: Joncțiuni interne
+##############################################################################
+## ultima actualizare: 2023-05-28
+
+library(tidyverse)
+library(lubridate)
+setwd('/Users/marinfotache/OneDrive/POCU-860-3-12-143072/7 Importul și prelucrarea datelor în limbajul R')
+load("chinook.RData")
+
+
+##############################################################################
+##                  Care sunt albumele formației `U2`
+##############################################################################
+
+# sol 1.1 - NATURAL JOIN
+temp <- artist %>%
+      filter (name == 'U2') %>%
+      inner_join(album)
+
+# sol 1.2 - NATURAL JOIN
+temp <- artist %>%
+     inner_join(album) %>%
+     filter (name == 'U2')
+
+
+# ##sol. 2
+temp <- artist %>%
+     filter (name == 'U2') %>%
+     inner_join(album, by = c('artistid' = 'artistid'))
+
+
+##############################################################################
+##		Care sunt piesele de pe albumul `Achtung Baby` al formatiei U2?
+##############################################################################
+
+
+# sol 0 - ERONATA!!!
+temp <- artist %>%
+     inner_join(album) %>%
+     inner_join(track) %>%
+     filter (name == 'U2' & title == 'Achtung Baby')
+
+# sol 1 - NATURAL JOIN + ECHI-JOIN
+temp <- artist %>%
+     inner_join(album) %>%
+     inner_join(track, by = c('albumid' = 'albumid')) %>%
+     filter (name.x == 'U2' & title == 'Achtung Baby')
+
+
+# sol 2 - NATURAL JOIN + rename
+temp <- artist %>%
+     rename(artist_name = name) %>%
+     inner_join(album) %>%
+     inner_join(track) %>%
+     filter (artist_name == 'U2' & title == 'Achtung Baby')
+
+
+# sol 3 - NATURAL JOIN + select
+temp <- artist %>%
+     filter (name == 'U2') %>%
+     select (-name) %>%
+     inner_join(album) %>%
+     inner_join(track) %>%
+     filter (title == 'Achtung Baby')
+
+
+# sol 4 - NATURAL JOIN + select + rename
+temp <- artist %>%
+     filter (name == 'U2') %>%
+     inner_join(album) %>%
+     inner_join(track %>%
+                     rename (track_name = name)
+                ) %>%
+     filter (title == 'Achtung Baby')
+
+
+# sol 5 - NATURAL JOIN + transmute + rename
+temp <- artist %>%
+     filter (name == 'U2') %>%
+     transmute (artistid) %>%
+     inner_join(album) %>%
+     filter (title == 'Achtung Baby') %>%
+     transmute (albumid) %>%
+     inner_join(track %>%
+                     rename (track_name = name)
+                )
+
+
+
+###########################################
+# introd to semi_join
+
+# inner join (natural)
+temp <- album %>%
+    inner_join(artist)
+
+
+# semi join
+temp <- album %>%
+    semi_join(artist)
+
+temp <- album %>%
+    inner_join(artist) %>%
+    select(albumid:artistid)
+
+
+# inner join (natural)
+temp <- artist %>%
+    inner_join(album)
+
+
+# semi join
+temp <- artist %>%
+    semi_join(album)
+
+###########################################
+
+
+# sol 6 - SEMI-JOIN
+temp <- track %>%
+     semi_join(
+          album %>%
+               filter (title == 'Achtung Baby') %>%
+          semi_join(artist %>%
+                         filter (name == 'U2')
+                    )
+     ) %>%
+     select(trackid:composer)
+
+
+
+##############################################################################
+##         Care sunt piesele formației `U2` vândute în anul 2013?
+##############################################################################
+temp <- track %>%
+             inner_join(album) %>%
+             transmute (track_name = name, trackid, album_title = title,
+                        artistid) %>%
+             inner_join(artist %>%
+                                filter (name == 'U2')) %>%
+             select (track_name:album_title) %>%
+             inner_join(invoiceline) %>%
+             inner_join(invoice) %>%
+             filter (lubridate::year(invoicedate) == 2013) %>%
+             select (track_name, album_title) %>%
+             arrange (track_name)
+
+
+
+############################################################################
+##          În ce țări s-a vândut muzica formației `Led Zeppelin`?
+############################################################################
+
+temp <- artist %>%
+    filter (name == 'Led Zeppelin') %>%
+    select (artistid) %>%
+    inner_join(album) %>%
+    transmute (album_title = title, albumid) %>%
+    inner_join(track) %>%
+    transmute (track_name = name, album_title, trackid) %>%
+    inner_join(invoiceline) %>%
+    inner_join(invoice) %>%
+    inner_join(customer) %>%
+    distinct (country) %>%
+    arrange(country)
+
+
+
+############################################################################
+##   Care sunt piesele formatiei `Led Zeppelin` la care, printre autori,
+##               se numara bateristul `John Bonham`
+############################################################################
+
+
+temp <- artist %>%
+      filter (name == 'Led Zeppelin') %>%
+      select (artistid) %>%
+      inner_join(album) %>%
+      transmute (album_title = title, albumid) %>%
+      inner_join(track) %>%
+      filter (str_detect(composer, 'Bonham'))
+
+
+
+
+############################################################################
+### 					AUTOJONCTIUNE / SELF-JOIN
+############################################################################
+
+##############################################################################
+##			Care sunt celelalte albume ale formatiei care a lansat
+##                   albumul 'Achtung Baby'?
+##############################################################################
+
+# ##solutie care `emuleaza` AUTO JOIN-ul din SQL
+
+# sol 1 - NATURAL JOIN + select
+temp <- album %>%
+     filter (title == 'Achtung Baby') %>%
+     select (artistid) %>%
+     inner_join(album)
+
+
+# sol 2 - ECHI-JOIN + transmute
+temp <- album %>%
+     filter (title == 'Achtung Baby') %>%
+     inner_join(album, by = c('artistid' = 'artistid')) %>%
+     transmute(artistid, albumid = albumid.y, title = title.y)
+
+
+
+##############################################################################
+##			Care sunt celelalte piese de pe albumul pe care apare piesa
+##             'For Those About To Rock (We Salute You)'?
+##############################################################################
+
+temp <- track %>%
+    inner_join(track, by = c('albumid' = 'albumid') ) %>%
+    filter (name.y == 'For Those About To Rock (We Salute You)') %>%
+    select (trackid.x:unitprice.x)
+
+
+
+##############################################################################
+##	Care este șeful angajatului cu numele (lastname) 'Johnson'
+##     și prenummele (firstname) 'Steve'
+##############################################################################
+
+# sol . 1
+temp <- employee %>%
+        filter (lastname == 'Johnson' & firstname == 'Steve') %>%
+        inner_join(employee, by = c('reportsto' = 'employeeid')) %>%
+        select (lastname.y, firstname.y)
+
+# sol . 2
+temp <- employee %>%
+        filter (lastname == 'Johnson' & firstname == 'Steve') %>%
+        transmute (employeeid = reportsto) %>%
+        inner_join(employee)
+
+# sol . 3
+temp <- employee %>%
+        filter (lastname == 'Johnson' & firstname == 'Steve') %>%
+        select (reportsto) %>%
+        inner_join(employee, by = c('reportsto' = 'employeeid'))
+
+
+
+
+#
+# ##############################################################################
+# ##               Probleme de rezolvat la curs/laborator/acasa
+# ##############################################################################
+#
+#
+# ##Afisare piesele si artistii din playlistul `Heavy Metal Classic`
+#
+#
+# ##Care sunt piesele formatiei `Led Zeppelin` compuse de cel putin trei muzicieni?
+#
+#
+###Care sunt piesele formatiei `Led Zeppelin` la care, printre compozitori, nu apare
+#--	`Robert Plant`
+#
+###Care sunt piesele formatiei `Led Zeppelin` la care, printre compozitori, nu apare
+#--	nici `Robert Plant`, nici `Jimmy Page`
+
+##############################################################################
+##	   Care sunt clientii din aceeasi tara ca si clientul `Robert Brown`
+##############################################################################
+##	   Extract customers from the same country as customer `Robert Brown`
+##############################################################################
---
+++ 7 Importul și prelucrarea datelor în limbajul R/03c3_chinook_03_tidyverse_union_intersect_difference.R
@@ -0,0 +1,351 @@
+############################################################################
+###      Titlul proiectului: Educație și dezvoltare în era digitală      ###
+###                   Cod proiect: POCU/860/3/12/143072                  ###
+###         Curs: Introducere în analiza datelor de mari dimensiuni      ###
+###                       Perioada: mai-iunie 2023                       ###
+############################################################################
+
+##############################################################################
+##        Studiu de caz: Interogări tidyverse pentru baza de date `chinook`
+##############################################################################
+## 			tidyverse03: Operatori ansamblisti (UNION, INTERSECT, EXCEPT)
+##############################################################################
+## ultima actualizare: 2023-05-28
+
+library(tidyverse)
+library(lubridate)
+
+setwd('/Users/marinfotache/OneDrive/POCU-860-3-12-143072/7 Importul și prelucrarea datelor în limbajul R')
+load("chinook.RData")
+
+
+###############################################################################
+###                                      UNION
+###############################################################################
+
+
+##############################################################################
+##			Care sunt piesele care apar pe două discuri ale formației
+##				'Iron Maiden', `Fear Of The Dark` și `A Real Live One`
+##############################################################################
+
+# solutia urmatoare nu elimina dublurile
+temp <- artist %>%
+     filter (name == 'Iron Maiden') %>%
+     select (-name) %>%
+     inner_join(album) %>%
+     filter (title == 'Fear Of The Dark' | title == 'A Real Live One') %>%
+     inner_join(track) %>%
+     select (name) %>%
+     arrange(name)
+
+# solutia urmatoare elimina dublurile
+temp <- artist %>%
+     filter (name == 'Iron Maiden') %>%
+     select (-name) %>%
+     inner_join(album) %>%
+     filter (title == 'Fear Of The Dark' | title == 'A Real Live One') %>%
+     inner_join(track) %>%
+     distinct (name) %>%
+     arrange(name)
+
+
+# sol. 1 cu `union`
+temp <- dplyr::union(
+     artist %>%
+          filter (name == 'Iron Maiden') %>%
+          select (-name) %>%
+          inner_join(album) %>%
+          filter (title == 'Fear Of The Dark') %>%
+          inner_join(track) %>%
+          distinct (name),
+     artist %>%
+          filter (name == 'Iron Maiden') %>%
+          select (-name) %>%
+          inner_join(album) %>%
+          filter (title == 'A Real Live One') %>%
+          inner_join(track) %>%
+          distinct (name)) %>%
+     arrange(name)
+
+
+# sol. 2 cu `union`
+temp <- artist %>%
+          filter (name == 'Iron Maiden') %>%
+          select (-name) %>%
+          inner_join(album) %>%
+          filter (title == 'Fear Of The Dark') %>%
+          inner_join(track) %>%
+          distinct (name) %>%
+     dplyr::union(
+          artist %>%
+               filter (name == 'Iron Maiden') %>%
+               select (-name) %>%
+               inner_join(album) %>%
+               filter (title == 'A Real Live One') %>%
+               inner_join(track) %>%
+               distinct (name)
+          ) %>%
+     arrange(name)
+
+
+##############################################################################
+## Care sunt subordonatii de ordinul 1 (directi) si 2 (subordonatii directi ai
+## subordonatilor de ordinul 1) ai lui `Adams` (lastname) `Andrew` (firstname)
+##############################################################################
+
+# solutie bazata pe `union`
+temp <-
+        # subordonatii de ordinul I
+        employee %>%
+                filter (lastname == 'Adams' & firstname == 'Andrew') %>%
+                transmute (boss_employeeid = employeeid) %>%
+                inner_join(employee, by = c('boss_employeeid' = 'reportsto')) %>%
+                select (-boss_employeeid) %>%
+        dplyr::union( # subordonatii de ordinul II
+                employee %>%
+                        filter (lastname == 'Adams' & firstname == 'Andrew') %>%
+                        transmute (boss_employeeid = employeeid) %>%
+                        inner_join(employee, by = c('boss_employeeid' = 'reportsto')) %>%
+                        transmute (first_order_subordinates_id = employeeid) %>%
+                        inner_join(employee, by = c('first_order_subordinates_id' = 'reportsto')) %>%
+                        select (-first_order_subordinates_id)
+        )
+
+
+
+# solutie bazata pe `bind_rows`
+temp <- bind_rows(
+        # subordonatii de ordinul I
+        employee %>%
+                filter (lastname == 'Adams' & firstname == 'Andrew') %>%
+                transmute (boss_employeeid = employeeid) %>%
+                inner_join(employee, by = c('boss_employeeid' = 'reportsto')) %>%
+                select (-boss_employeeid),
+        # subordonarii de ordinul II
+        employee %>%
+                filter (lastname == 'Adams' & firstname == 'Andrew') %>%
+                transmute (boss_employeeid = employeeid) %>%
+                inner_join(employee, by = c('boss_employeeid' = 'reportsto')) %>% #subordonatii directi
+                transmute (first_order_subordinates_id = employeeid) %>%
+                inner_join(employee, by = c('first_order_subordinates_id' = 'reportsto')) %>%
+                select (-first_order_subordinates_id)
+        )
+
+
+
+
+
+##############################################################################
+##                                INTERSECT
+##############################################################################
+
+##############################################################################
+##			Care sunt piesele comune (cu acelasi titlu) de pe
+##			albumele `Fear Of The Dark` si `A Real Live One`
+##					ale formatiei 'Iron Maiden'
+##############################################################################
+
+# solutie eronata 1 !!! (AND)
+temp <- artist %>%
+          filter (name == 'Iron Maiden') %>%
+          select (-name) %>%
+          inner_join(album) %>%
+          filter (title == 'Fear Of The Dark' & title == 'A Real Live One') %>%
+          inner_join(track) %>%
+          select (name)
+
+# solutie eronata 2 !!! (OR)
+temp <- artist %>%
+          filter (name == 'Iron Maiden') %>%
+          select (-name) %>%
+          inner_join(album) %>%
+          filter (title == 'Fear Of The Dark' | title == 'A Real Live One') %>%
+          inner_join(track) %>%
+          select (name)
+
+
+# solutie corecta bazata pe `intersect`
+temp <- dplyr::intersect(
+     artist %>%
+          filter (name == 'Iron Maiden') %>%
+          select (-name) %>%
+          inner_join(album) %>%
+          filter (title == 'Fear Of The Dark') %>%
+          inner_join(track) %>%
+          select (name),
+     artist %>%
+          filter (name == 'Iron Maiden') %>%
+          select (-name) %>%
+          inner_join(album) %>%
+          filter (title == 'A Real Live One') %>%
+          inner_join(track) %>%
+          select (name)
+     )
+
+
+# solutie corecta bazata pe `intersect`
+temp <- artist %>%
+          filter (name == 'Iron Maiden') %>%
+          select (-name) %>%
+          inner_join(album) %>%
+          filter (title == 'Fear Of The Dark') %>%
+          inner_join(track) %>%
+          select (name) %>%
+        dplyr::intersect(
+             artist %>%
+                filter (name == 'Iron Maiden') %>%
+                select (-name) %>%
+                inner_join(album) %>%
+                filter (title == 'A Real Live One') %>%
+                inner_join(track) %>%
+                select (name)
+                        )
+
+
+# ##solutie bazata de auto-join
+temp <- artist %>%
+          filter (name == 'Iron Maiden') %>%
+          select (-name) %>%
+          inner_join(album) %>%
+          filter (title == 'Fear Of The Dark') %>%
+          inner_join(track) %>%
+          select (name) %>%
+     inner_join(
+          artist %>%
+               filter (name == 'Iron Maiden') %>%
+               select (-name) %>%
+               inner_join(album) %>%
+               filter (title == 'A Real Live One') %>%
+               inner_join(track) %>%
+               select (name)
+               )
+
+
+
+# solutie noua (fara echivalent in SQL-PostgreSQL): SEMI JOIN
+temp <- track %>%
+     semi_join(
+          album %>%
+               filter (title == 'Fear Of The Dark') %>%
+               semi_join(
+                    artist %>%
+                         filter (name == 'Iron Maiden')
+                         )) %>%
+     select (name) %>%
+     semi_join(
+          track %>%
+               semi_join(
+                    album %>%
+                         filter (title == 'A Real Live One') %>%
+                    semi_join(
+                         artist %>%
+                              filter (name == 'Iron Maiden')
+                              ))
+     )
+
+
+
+##############################################################################
+##                                 EXCEPT
+##############################################################################
+
+##############################################################################
+## Care sunt piesele formatiei 'Iron Maiden' de pe albumul `Fear Of The Dark`
+##				care NU apar si pe albumul `A Real Live One`
+##############################################################################
+
+
+# solutie bazata pe `setdiff` (exchivalentul EXCEPT)
+temp <- dplyr::setdiff(
+     artist %>%
+          filter (name == 'Iron Maiden') %>%
+          select (-name) %>%
+          inner_join(album) %>%
+          filter (title == 'Fear Of The Dark') %>%
+          inner_join(track) %>%
+          select (name),
+     artist %>%
+          filter (name == 'Iron Maiden') %>%
+          select (-name) %>%
+          inner_join(album) %>%
+          filter (title == 'A Real Live One') %>%
+          inner_join(track) %>%
+          select (name)
+     )
+
+
+# solutie noua (fara echivalent in SQL-PostgreSQL): ANTI JOIN
+temp <- track %>%
+     semi_join(
+          album %>%
+               filter (title == 'Fear Of The Dark') %>%
+               semi_join(
+                    artist %>%
+                         filter (name == 'Iron Maiden')
+                         )) %>%
+     select (name) %>%
+     anti_join(
+          track %>%
+               semi_join(
+                    album %>%
+                         filter (title == 'A Real Live One') %>%
+                    semi_join(
+                         artist %>%
+                              filter (name == 'Iron Maiden')
+                              ))
+     )
+
+
+
+##############################################################################
+##  Care sunt piesele formatiei `Led Zeppelin` la care, printre compozitori,
+##          nu apare nici `Robert Plant`, nici `Jimmy Page`
+##############################################################################
+
+temp <- artist %>%
+        filter (name == 'Led Zeppelin') %>%
+        transmute(artistid, artist_name = name) %>%
+        inner_join(album) %>%
+        inner_join(track) %>%
+        anti_join(
+                artist %>%
+                filter (name == 'Led Zeppelin') %>%
+                transmute(artistid, artist_name = name) %>%
+                inner_join(album) %>%
+                inner_join(track) %>%
+                filter (str_detect(composer, 'Plant'))) %>%
+        anti_join(
+                artist %>%
+                filter (name == 'Led Zeppelin') %>%
+                transmute(artistid, artist_name = name) %>%
+                inner_join(album) %>%
+                inner_join(track) %>%
+                filter (str_detect(composer, 'Page')))
+
+
+
+
+
+##############################################################################
+##               Probleme de rezolvat la curs/laborator/acasa
+##############################################################################
+#
+#
+# Afisare piesele (si artistii) comune playlisturilor `Heavy Metal Classic` si `Music`
+#
+# Care sunt piesele formatiei `Led Zeppelin` compuse numai de `Robert Plant`
+#
+# Care sunt piesele formatiei `Led Zeppelin` compuse, impreuna, de `Robert Plant` si
+#  `Jimmy Page`, cu sau fara alti colegi/muzicieni?
+#
+# Care sunt piesele formatiei `Led Zeppelin` compuse, impreuna, de `Robert Plant` si
+#  `Jimmy Page`, fara alti colegi/muzicieni?
+#
+# Care sunt piesele formatiei `Led Zeppelin` la care, printre compozitori, nu apare
+# 	`Robert Plant`
+#
+
+##############################################################################
+##             La ce întrebări răspund următoarele interogări ?
+##############################################################################
